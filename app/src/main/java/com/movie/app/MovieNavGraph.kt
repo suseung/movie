2c7f359cf32ad470afa4e794ac5f15be08a2030c
suseung <seungsu.lee@flitto.com>
2024-10-21 18:18:47 +0900

    feat: MovieNavGraph 추가 작업

---
+++ app/src/main/java/com/movie/app/MovieNavGraph.kt
@@ -0,0 +1,37 @@
+package com.movie.app
+
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.movie.app.Destination.BOOKMARK
+import com.movie.app.Destination.HOME
+
+object Destination {
+    const val HOME = "home"
+    const val BOOKMARK = "bookmark"
+}
+
+@Composable
+fun MovieNavGraph(
+    navController: NavHostController = rememberNavController()
+) {
+    val startDestination by remember { mutableStateOf(HOME) }
+
+    NavHost(
+        modifier = Modifier.background(MaterialTheme.colorScheme.background),
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(HOME) {}
+        composable(BOOKMARK) {}
+    }
+}
