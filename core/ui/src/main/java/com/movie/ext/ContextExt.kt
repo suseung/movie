1c8fc3f4de520685475f2eaafa6e1c7bf87f26cb
suseung <seungsu.lee@flitto.com>
2024-10-22 17:36:37 +0900

    feat: MovieScaffold, MovieTopAppBar 추가

--- core/ui/src/main/java/com/movie/ext/ContextExt.kt
+++ core/ui/src/main/java/com/movie/ext/ContextExt.kt
@@ -4,4 +4,4 @@ import android.content.Context
 import android.widget.Toast
 
 fun Context.toastS(message: String) =
-    Toast.makeText(this, message ?: "", Toast.LENGTH_SHORT).show()
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
